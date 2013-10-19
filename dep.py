41d470c2e988141fc46a1707ac210e5c4dac191f
Mark Feltner <feltner.mj@gmail.com>
2013-10-19 16:46:46 -0500

    feat: bash_colors script

--- dep.py
+++ dep.py
@@ -1,27 +1,39 @@
 #!/usr/bin/env python
 
+# Lil' python script to download other scripts and useful things.
+
+# @TODO:
+# * github urls
+
 import sys
+import urlparse
 import urllib2
 
-# url
-# @TODO (mark): github urls
 DEPENDENCIES = (
-    ('git-wtf', 'https://raw.github.com/michaelklishin/git-wtf/master/git-wtf'),
-    )
+    ('git-wtf', 'https://raw.github.com/michaelklishin/git-wtf/master/git-wtf')
+  , ('leiningen', 'https://raw.github.com/technomancy/leiningen/stable/bin/lein')
+  , ('prettyping', 'https://bitbucket.org/denilsonsa/small_scripts/raw/0c59d14ca5f1aac01447e28d81f5d5c433976348/prettyping.sh')
+  , ('bash_colors', 'https://raw.github.com/maxtsepkov/bash_colors/master/bash_colors.sh')
+)
 
 def parse_url(url):
-    return url
+    u = urlparse.urlparse(url)
+
+    if not u.scheme:
+        if url.count('/') == 1 and len(url.split('/')) == 2:
+            url = url.strip()
+            url = "https://raw.github.com/" + url
+            u = urlparse.urlparse(u)
+    return urlparse.urlunparse(u)
 
 def main(argv=None):
-    
+
     for name, url in DEPENDENCIES:
-        print("""
-        > %s 
-        %s""" % (name, url))
-        content = urllib2.urlopen(url).readlines()
+        print("""> %s
+                %s""" % (name, url))
+        content = urllib2.urlopen(parse_url(url)).readlines()
         with open(name, 'w') as fil:
             fil.writelines(content)
-
 
     return 0 # success
 
