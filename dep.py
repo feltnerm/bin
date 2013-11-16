f1e71621218ae155336d172a94b73108c3d1d2fc
Mark Feltner <feltner.mj@gmail.com>
2013-11-16 15:01:16 -0600

    feat: SpotifyControl, z, zshdb, git-friendly

--- dep.py
+++ dep.py
@@ -5,6 +5,8 @@
 # @TODO:
 # * github urls
 
+import os
+import stat
 import sys
 import urlparse
 import urllib2
@@ -14,7 +16,14 @@ DEPENDENCIES = (
   , ('leiningen', 'https://raw.github.com/technomancy/leiningen/stable/bin/lein')
   , ('prettyping', 'https://bitbucket.org/denilsonsa/small_scripts/raw/0c59d14ca5f1aac01447e28d81f5d5c433976348/prettyping.sh')
   , ('bash_colors', 'https://raw.github.com/maxtsepkov/bash_colors/master/bash_colors.sh')
+  , ('z', 'https://raw.github.com/rupa/z/master/z.sh')
 )
+
+def chmod(file, mode):
+    st = os.stat(name)
+    os.chmod(name, st.st_mode | mode)
+
+chmod_x = lambda file: chmod(file, stat.S_IEXEC)
 
 def parse_url(url):
     u = urlparse.urlparse(url)
@@ -34,6 +43,7 @@ def main(argv=None):
         content = urllib2.urlopen(parse_url(url)).readlines()
         with open(name, 'w') as fil:
             fil.writelines(content)
+        chmod_x(name)
 
     return 0 # success
 
